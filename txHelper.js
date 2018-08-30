91f2fddf54c2a3ebdd68daee5ae2547fdd1c690e
Radek Ostrowski <radek@startonchain.com>
2018-08-31 00:18:22 +1000

    small cleanup

--- txHelper.js
+++ txHelper.js
@@ -5,11 +5,14 @@ var Web3 = require('web3');
 //ropsten
 var web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io/vCE9q890Is08T4xq5qKm'));
 
+let personalWalletAddress = "0x0dcd2f752394c41875e259e00bb44fd505297caf";
 let privateKey = Buffer.from('2e5a537948b5d4dd63f690f5a82f8591cb5c41a562c9cce850adfb29a99a8cc5', 'hex');
 let publicAddress = "0x9E48c4A74D618a567CD657579B728774f35B82C5";
-let personalWalletAddress = "0x0dcd2f752394c41875e259e00bb44fd505297caf";
-
 let toAddress = "0xd4a0d9531Bf28C26869C526b2cAd2F2eB77D3844";
+let ethAmountInWei = 1;
+let data = "0x00"; //this will be different for token transfer or any other contract function call
+let rewardType = "0x0000000000000000000000000000000000000000"; // address(0) for ether or token address for erc20
+let rewardAward = 0;
 
 var preparePayload = async function(targetWallet, from, to, value, data, rewardType, rewardAmount) {
     let personalWalletABI = [{"constant":false,"inputs":[{"name":"_v","type":"uint8"},{"name":"_r","type":"bytes32"},{"name":"_s","type":"bytes32"},{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"},{"name":"_data","type":"bytes"},{"name":"_rewardType","type":"address"},{"name":"_rewardAmount","type":"uint256"}],"name":"execute","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"account","type":"address"}],"name":"isActionAccount","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"nonces","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"account","type":"address"}],"name":"isMasterAccount","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"roles","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"login","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[{"name":"masterAccount","type":"address"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"payable":true,"stateMutability":"payable","type":"fallback"}];
@@ -41,4 +44,4 @@ var preparePayload = async function(targetWallet, from, to, value, data, rewardT
     console.log(JSON.stringify(payload));
 }
 
-preparePayload(personalWalletAddress, publicAddress, toAddress, 1, "0x00", "0x0000000000000000000000000000000000000000", 0);
+preparePayload(personalWalletAddress, publicAddress, toAddress, ethAmountInWei, data, rewardType, rewardAward);
